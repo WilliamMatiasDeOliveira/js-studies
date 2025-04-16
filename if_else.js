ea20a55fa9575d115a79a32b744a594a310dbab9
William Matis de Oliveira <williammatiasjau@hotmail.com>
2025-04-16 20:29:09 -0300

    if else

---
+++ if_else.js
@@ -0,0 +1,25 @@
+// estrutura de decisão if, if else
+let num = 10
+
+// exemplo com apenas if
+if(num > 10){
+    console.log("MAIOR")
+}
+
+console.log("MENOR");
+
+// exemplo com if e else
+if(num > 10){
+    console.log("MAIOR")
+}else{
+    console.log("MENOR");
+}
+
+// exemplo com if, else if, else
+if(num > 10){
+    console.log("MAIOR")
+}else if(num == 10){
+    console.log("IGUAL");
+}else{
+    console.log("MENOR");
+}
