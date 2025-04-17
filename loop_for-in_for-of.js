59e92e190ade40127d5a3924094e598ffb904f62
William Matis de Oliveira <williammatiasjau@hotmail.com>
2025-04-16 21:31:36 -0300

    for in e for of

---
+++ loop_for-in_for-of.js
@@ -0,0 +1,17 @@
+let array = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+// exemplo de for in
+// O n vai retornar a posição do elemento
+for(n in array){
+    console.log(n);
+}
+
+// exemplo de for of
+// O n retorna o elemento do array
+for(n of array){
+    console.log(n);
+    
+}
+
+// Resumo o for in acessa a posição do elemento dentro de um array
+// O for of acessa o elemento em si
